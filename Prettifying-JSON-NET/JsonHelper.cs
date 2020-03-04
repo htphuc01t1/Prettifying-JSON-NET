f870b4766cb2eda075b6806a441822c55cfeab41
htphuc01t1 <htphuc01t1@gmail.com>
2020-03-04 15:37:35 +0700

    Init project
    Init project

---
+++ Prettifying-JSON-NET/JsonHelper.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+
+namespace Prettifying_JSON_NET
+{
+    class JsonHelper
+    {
+        public static string Indent = "    ";
+
+        public static string FormatJson(string jsonString)
+        {
+            var output = new StringBuilder(jsonString.Length * 2);
+            char? quote = null;
+            int depth = 0;
+
+            for (int i = 0; i < jsonString.Length; ++i)
+            {
+                char charactor = jsonString[i];
+
+                switch (charactor)
+                {
+                    case '{':
+                    case '[':
+                        output.Append(charactor);
+                        if (!quote.HasValue)
+                        {
+                            output.AppendLine();
+                            output.Append(Indent.Repeat(++depth));
+                        }
+                        break;
+                    case '}':
+                    case ']':
+                        if (quote.HasValue)
+                            output.Append(charactor);
+                        else
+                        {
+                            output.AppendLine();
+                            output.Append(Indent.Repeat(--depth));
+                            output.Append(charactor);
+                        }
+                        break;
+                    case '"':
+                    case '\'':
+                        output.Append(charactor);
+                        if (quote.HasValue)
+                        {
+                            if (!output.IsEscaped(i))
+                                quote = null;
+                        }
+                        else quote = charactor;
+                        break;
+                    case ',':
+                        output.Append(charactor);
+                        if (!quote.HasValue)
+                        {
+                            output.AppendLine();
+                            output.Append(Indent.Repeat(depth));
+                        }
+                        break;
+                    case ':':
+                        if (quote.HasValue) output.Append(charactor);
+                        else output.Append(" : ");
+                        break;
+                    default:
+                        if (quote.HasValue || !char.IsWhiteSpace(charactor))
+                            output.Append(charactor);
+                        break;
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
+
+static class Extensions
+{
+    public static bool IsEscaped(this string str, int index)
+    {
+        bool escaped = false;
+        while (index > 0 && str[--index] == '\\') escaped = !escaped;
+        return escaped;
+    }
+    public static string Repeat(this string str, int count)
+    {
+        return new StringBuilder().Insert(0, str, count).ToString();
+    }   
+
+    public static bool IsEscaped(this StringBuilder str, int index)
+    {
+        return str.ToString().IsEscaped(index);
+    }
+}
---
+++ Prettifying-JSON-NET/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Prettifying_JSON_NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string jsonText = System.IO.File.ReadAllText("..\\..\\json_input.txt");
+            string jsonPrettifyingText = JsonHelper.FormatJson(jsonText);
+            System.IO.File.WriteAllText(@"..\\..\\json_output.txt", jsonPrettifyingText);
+
+            Console.WriteLine("Prettifying JSON string was successfully");
+            Console.WriteLine("Open and check prettifying JSON string on json_output.txt file");
+            Console.ReadLine();
+        }
+    }
+}
